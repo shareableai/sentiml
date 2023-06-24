78b44ffb25ed4f8ffbd9294af448553193aaf44c
Lissa Hyacinth <lissahyacinth@gmail.com>
2023-06-24 21:49:11 +0100

    Update FB object detection to include tracked classes.

--- examples/fb-object-detection.py
+++ examples/fb-object-detection.py
@@ -5,29 +5,33 @@ import requests
 
 from observer.trackers import Observer
 from observer.tracking_type import TrackingType
+from observer.track_class import track_class
 
 
-url = "http://images.cocodataset.org/val2017/000000039769.jpg"
-image = Image.open(requests.get(url, stream=True).raw)
+if __name__ == "__main__":
+    url = "http://images.cocodataset.org/val2017/000000039769.jpg"
+    image = Image.open(requests.get(url, stream=True).raw)
 
-processor = DetrImageProcessor.from_pretrained("facebook/detr-resnet-50")
-model = DetrForObjectDetection.from_pretrained("facebook/detr-resnet-50")
+    processor = DetrImageProcessor.from_pretrained("facebook/detr-resnet-50")
+    track_class(processor)
+    model = DetrForObjectDetection.from_pretrained("facebook/detr-resnet-50")
+    track_class(model)
 
-Observer.track(TrackingType.Inference)
-inputs = processor(images=image, return_tensors="pt")
-outputs = model(**inputs)
-# convert outputs (bounding boxes and class logits) to COCO API
-# let's only keep detections with score > 0.9
-target_sizes = torch.tensor([image.size[::-1]])
-results = processor.post_process_object_detection(
-    outputs, target_sizes=target_sizes, threshold=0.9
-)[0]
+    Observer.track(TrackingType.Inference)
+    inputs = processor(images=image, return_tensors="pt")
+    outputs = model(**inputs)
+    # convert outputs (bounding boxes and class logits) to COCO API
+    # let's only keep detections with score > 0.9
+    target_sizes = torch.tensor([image.size[::-1]])
+    results = processor.post_process_object_detection(
+        outputs, target_sizes=target_sizes, threshold=0.9
+    )[0]
 
-for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
-    box = [round(i, 2) for i in box.tolist()]
-    print(
-        f"Detected {model.config.id2label[label.item()]} with confidence "
-        f"{round(score.item(), 3)} at location {box}"
-    )
-Observer.stop()
+    for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
+        box = [round(i, 2) for i in box.tolist()]
+        print(
+            f"Detected {model.config.id2label[label.item()]} with confidence "
+            f"{round(score.item(), 3)} at location {box}"
+        )
+    Observer.stop()
 
